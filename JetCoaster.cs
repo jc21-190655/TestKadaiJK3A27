66e44f74c3be196bb566fbbc034b3722f61365af
悠 道輪 <190655@jc-21.jp>
2022-02-04 12:59:27 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1_吉澤先生_
+{
+    public class JetCoaster
+
+    {
+        public static int ride(int k, int[] g, int r)
+        
+        {
+            //index 要素
+            //ridecount 残りの乗れる人数
+            int index = 0;
+            int totalcount = 0;
+            
+            for (int i =0;i < r; i++)
+
+            {
+                int ridecount = k;
+
+                for (int j = 0; j < g.Length; j++)
+                {
+                    if (ridecount >=g[index] )
+                    {
+                        ridecount=ridecount-g[index];
+                        index++;
+                        index = index % g.Length;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                totalcount = totalcount + (k - ridecount);
+            }
+
+
+            return totalcount;
+
+        }
+
+     }
+}
+
+    
+    
+
---
+++ kuriage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1_吉澤先生_
+{
+   public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+
+            if (i < 0)
+            {
+                i = i * -1;
+                if (i % 10 == 0)
+                {
+                    return i * -1;
+                }
+                else
+                {
+                    i = i - (i % 10) + 10;
+                    return i * -1;
+                }
+
+            }
+            else 
+            { 
+                if (i % 10 == 0)
+                {
+                    return i;
+                }
+                else
+                {
+                    i = i - (i % 10) + 10;
+                    return i;
+                }
+            }
+         
+        }
+    }
+}
